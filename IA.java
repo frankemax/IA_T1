4473c6b4a36bcd5cbe8c56a97da569de7b0aae2e
Lucas Schell <33852112+LRschell@users.noreply.github.com>
2020-05-01 19:05:10 -0300

    Add files via upload

---
+++ IA.java
@@ -0,0 +1,30 @@
+package ia;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class IA {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("lab10.txt");
+        Scanner sc = new Scanner(f);
+        
+        int tam = sc.nextInt();
+        
+        String[][] labirinto = new String[tam][tam];
+        
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                labirinto[i][j] = sc.next();
+            }
+        }
+        
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                System.out.print(labirinto[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
