b9c6d2af86f93f813bccd14032fd6799a965078a
frankemax <42683590+frankemax@users.noreply.github.com>
2020-05-02 00:53:21 -0300

    Update Labirinto.java

--- Labirinto.java
+++ Labirinto.java
@@ -6,22 +6,22 @@ import java.util.Scanner;
 
 public class Labirinto {
     private String[][] lab;
-    
+
     public Labirinto() throws FileNotFoundException{
         File f = new File("lab10.txt");
         Scanner sc = new Scanner(f);
-        
+
         int tam = sc.nextInt();
-        
-        String[][] labirinto = new String[tam][tam];
-        
+
+        lab = new String[tam][tam];
+
         for (int i = 0; i < tam; i++) {
             for (int j = 0; j < tam; j++) {
-                labirinto[i][j] = sc.next();
+                lab[i][j] = sc.next();
             }
         }
     }
-    
+
     public String[][] getLabirinto(){
         return lab;
     }
