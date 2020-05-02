7bd548cdf53051cce4afa323d2abf6f1f6e5a8ae
frankemax <42683590+frankemax@users.noreply.github.com>
2020-05-02 00:53:40 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,14 @@
+package ia;
+
 import java.io.FileNotFoundException;
 
-public class main {
+public class App {
 
     public static void main(String[] args) throws FileNotFoundException {
         Labirinto labirinto = new Labirinto();
         String lab[][] = labirinto.getLabirinto();
-        
-        for (int i = 0; i < lab.length; i++) {
+
+        for (int i = 0; i < lab[0].length; i++) {
             for (int j = 0; j < lab[0].length; j++) {
                 System.out.print(lab[i][j]+" ");
             }
