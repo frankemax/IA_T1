1bd49c057bbcb156b0e7a440732c868d6c1dad5a
Lotusdigital <42621099+Lotusdigital398@users.noreply.github.com>
2020-05-07 23:31:51 -0300

    Generalização do App.java

--- ia/App.java
+++ ia/App.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 public class App {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Genetico gen = new Genetico();
+        Genetico gen = new Genetico(3,2000,20,50);
 
     }
 }
