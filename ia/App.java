c7755467d7abc9f7eeae100843aeb8699faf564a
frankemax <42683590+frankemax@users.noreply.github.com>
2020-05-08 20:09:06 -0300

    Update App.java

--- ia/App.java
+++ ia/App.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 public class App {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Genetico gen = new Genetico(3,2000,20,50);
+        Genetico gen = new Genetico(0.5,2000,200,50);
 
     }
 }
