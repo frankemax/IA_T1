b41e16de266903a0e45636f3ee10be87e2dd8e41
Lucas Schell <33852112+LRschell@users.noreply.github.com>
2020-05-09 04:18:39 -0300

    Update App.java

--- ia/App.java
+++ ia/App.java
@@ -5,7 +5,9 @@ import java.io.FileNotFoundException;
 public class App {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Genetico gen = new Genetico(0.5,2000,200,50);
-
+        Genetico gen = new Genetico(0.01,2000,20,50);
+        int[] in = {0,0};
+        int[] out = {18,17};
+        AStar a = new AStar(in,gen.end);
     }
 }
