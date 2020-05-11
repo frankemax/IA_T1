78399f47da236ccde3157914f94bde59de3f1e31
Lucas Schell <33852112+LRschell@users.noreply.github.com>
2020-05-11 04:27:07 -0300

    Update App.java

--- ia/App.java
+++ ia/App.java
@@ -1,13 +1,22 @@
 package ia;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class App {
 
     public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("saida.txt");
+        PrintWriter pw = new PrintWriter(f);
         Genetico gen = new Genetico(0.01,2000,20,50);
         int[] in = {0,0};
-        int[] out = {18,17};
         AStar a = new AStar(in,gen.end);
+        pw.print("Resultado:\n");
+        pw.print(gen.log);
+        pw.print(a.str);
+        System.out.println(gen.log);
+        System.out.println(a.str);
+        pw.close();
     }
 }
